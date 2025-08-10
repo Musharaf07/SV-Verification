0319f3c97fc362fece0925f286f8849a20d7ebe5
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-08-10 23:45:37 +0530

    Create design.sv

---
+++ 01. Full Adder Verification/design.sv
@@ -0,0 +1,9 @@
+//Full Adder Design Module
+module full_adder(
+  input  a, b, cin,
+  output sum, cout
+);
+  assign sum = a ^ b ^ cin;
+  assign cout = (a & b) | (b & cin) | (a & cin);
+
+endmodule
